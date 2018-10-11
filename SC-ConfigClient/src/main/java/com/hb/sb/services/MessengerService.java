da1e7b333b8f0d781dadd14ea7829c28e858cf99
hanubindh <h.b.krishna@gmail.com>
2018-10-11 11:51:54 +0530

    Updated SC-Config

--- SC-ConfigClient/src/main/java/com/hb/sb/services/MessengerService.java
+++ SC-ConfigClient/src/main/java/com/hb/sb/services/MessengerService.java
@@ -7,6 +7,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.cloud.context.config.annotation.RefreshScope; 
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
@@ -14,6 +15,7 @@ import org.springframework.stereotype.Component;
 @Component
 @RestController
 @RequestMapping("messenger")
+@RefreshScope
 public class MessengerService {
 
 	private static final Logger logger 
